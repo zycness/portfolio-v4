f3bedce0d05835e5ea4dfc0e24b4a24dec1dc2c2
Zycness <80533588+zycness@users.noreply.github.com>
2024-04-05 08:56:11 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button, Container } from '@mantine/core';
+import { HeaderSimple } from './components/HeaderSimple/HeaderSimple';
+
+function App() {
+  return (
+    <>
+      <HeaderSimple />
+      <Container size={'xl'}>
+        <Button color="red">asdasdsad</Button>
+      </Container>
+    </>
+  );
+}
+
+export default App;
